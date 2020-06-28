f11340f305364a0effbc934173d51c7d7931c696
Turgor69 <64898566+Turgor69@users.noreply.github.com>
2020-06-28 13:45:28 +0300

    Update snake.c

--- snake/snake.c
+++ snake/snake.c
@@ -14,6 +14,24 @@ struct food {
 	int value;
 };
 
+void printposition(int y, int x, char q) {
+	move(y, x); 
+	addch(q); 
+}
+
+void field(int max_Y, int max_X) { 
+	for (int i = 0; i < max_Y + 2; i++) {
+		printposition(i, 0, '0');
+		printposition(i, max_X + 1, '0');	
+	}
+	
+	for (int i = 0; i < max_X + 2; i++) {
+		printposition(0, i, '0');
+		printposition(max_Y + 1, i, '0');	
+	}
+	attron(COLOR_PAIR(1));
+}
+
 int main() {
 	initscr(); 	   
 	noecho(); 
@@ -23,6 +41,9 @@ int main() {
 	attron(COLOR_PAIR(2));
 	curs_set(0); 
 	nodelay(stdscr, TRUE);
+	
+	field(max_Y, max_X); 
+	
 	endwin();
 	return 0;
 }
