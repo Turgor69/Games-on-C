9e46e94e81c79fd0e220e24a39c1842016346da3
Turgor69 <64898566+Turgor69@users.noreply.github.com>
2020-06-28 13:44:13 +0300

    Update snake.c

--- snake/snake.c
+++ snake/snake.c
@@ -18,6 +18,11 @@ int main() {
 	initscr(); 	   
 	noecho(); 
 	start_color();
+	init_pair(1, COLOR_GREEN, COLOR_BLACK);
+	init_pair(1, COLOR_RED, COLOR_BLACK);
+	attron(COLOR_PAIR(2));
+	curs_set(0); 
+	nodelay(stdscr, TRUE);
 	endwin();
 	return 0;
 }
